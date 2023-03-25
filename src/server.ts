8707d47a22c0ace4658e87bd1e6ce73d4902b8ef
Ibragim <lol.3007@mail.ru>
2023-03-24 20:54:13 -0700

    default graphQL

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,15 @@
-import cors from 'cors';
-import express from 'express';
+import { ApolloServer } from 'apollo-server';
 import { PORT } from './config';
+import { context } from './context/context';
+import { resolvers } from './graphql/resolvers';
+import { typeDefs } from './graphql/schema';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
-export const app = express();
-const jsonMiddleWare = express.json();
-
-const whitelist = ['http://localhost:3000'];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || whitelist.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  })
-);
-
-app.use(jsonMiddleWare);
-app.use('/api', (req, res) => {
-  console.log('hello owlrd');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-app.listen(PORT, () => {
-  console.log('listen port: ' + PORT);
-});
+server.listen({ port: PORT }, () => console.log(`Listen port ${PORT}...`));
